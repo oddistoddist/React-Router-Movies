ca527bdba580582e3a06adf9ac560b817a0b83c7
Lee Scott <Odda@MacBook-Pro-2.local>
2021-08-19 14:26:21 -0400

    mvp

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import { Switch, Route, Redirect } from 'react-router-dom'
 
 import SavedList from './Movies/SavedList';
+import MovieList from './Movies/MovieList'
+import Movie from './Movies/Movie'
 
 export default function App () {
   const [saved, setSaved] = useState([]); // Stretch: the ids of "saved" movies
@@ -30,7 +33,16 @@ export default function App () {
     <div>
       <SavedList list={[ /* This is stretch */]} />
 
-      <div>Replace this Div with your Routes</div>
+      <Switch>
+        <Route exact path='/'>
+          <MovieList movies={movieList}/>
+        </Route>
+        <Route path='/movies/:id'>
+
+          <Movie />
+        </Route>
+        <Redirect to='/' />
+      </Switch>
     </div>
   );
 }
